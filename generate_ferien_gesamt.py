3be68ba1f3012ff24decd4a66f6d9b168fd4eb25
VTTghICS <ross@vtt.hamburg>
2026-04-23 15:49:16 +0200

    Create generate_ferien_gesamt.py

---
+++ generate_ferien_gesamt.py
@@ -0,0 +1,36 @@
+import requests
+from ics import Calendar
+from datetime import datetime
+
+urls = {
+    "Hamburg": "https://ics.tools/Ferien/hamburg.ics", #hier ics URLS einfügen und den Rest nicht anfassen!ics urls kopieren von https://ics.tools/ //
+    "Schleswig-Holstein": "https://ics.tools/Ferien/schleswig-holstein.ics",
+    "Niedersachsen": "https://ics.tools/Ferien/niedersachsen.ics",
+    "Mecklenburg-Vorpommern": "https://ics.tools/Ferien/mecklenburg-vorpommern.ics"
+}
+
+merged = Calendar()
+seen = set()
+
+for region, url in urls.items():
+    r = requests.get(url)
+    cal = Calendar(r.text)
+
+    for event in cal.events:
+        key = (event.name, event.begin.date(), event.end.date())
+
+        # Duplikate vermeiden
+        if key in seen:
+            continue
+        seen.add(key)
+
+        # Bundesland hinzufügen
+        event.name = f"{event.name} ({region})"
+
+        merged.events.add(event)
+
+# Datei speichern
+with open("ferien_gesamt.ics", "w", encoding="utf-8") as f:
+    f.writelines(merged)
+
+print("Fertig:", datetime.now())
